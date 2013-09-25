a05b659416fac4d78346321bb840f2ea6aaec6a0
Rohit Kumar <rgarg1992@gmail.com>
2013-09-26 04:44:32 +0530

    Course Home Logic

--- courses/urls.py
+++ courses/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import patterns,url
 from django.conf import settings
 
 urlpatterns = patterns('courses.views',
-    url(r'^registerCourse/','registerCourse',name="register_course_url"),
+    url(r'^RegisterCourse/','RegisterCourse',name="register_course_url"),
+    url(r'^course/(?P<id>\d+)','CourseHome',name="course_home_url"),
+    url(r'^course/(?P<id>\d+)/members','CourseMember',name="course_member_url"),
+
 )
--- courses/views.py
+++ courses/views.py
@@ -5,12 +5,12 @@ from courses.forms import CourseForm
 from courses.models import Course,CourseMembership
 from courses.signals import CreateCourseMembership 
 from django.contrib.auth.decorators import login_required
-
+from django.shortcuts import get_object_or_404,get_list_or_404
 
 #Course registration
 #permission create and check
 @login_required
-def registerCourse(request):
+def RegisterCourse(request):
   if request.method =='POST':
     form = CourseForm(request.POST,request.FILES)
     print "checking for file now"
@@ -21,10 +21,29 @@ def registerCourse(request):
       newCourse.save()
 
       CreateCourseMembership.send(sender = Course,instance=newCourse,user=request.user,created = True)
-      return redirect(settings.HOME) # Redirect after POST
+      return redirect(reverse('course_home_url', args(newCourse.id,))) # Redirect after POST
   else:
     form = CourseForm() # An unbound form
 
   return render_to_response('courses/register.html', {
     'form': form,
 },context_instance=RequestContext(request))
+
+
+#decide on membership policy- members only view....or all with restricted access
+@login_required
+def CourseHome(request,id):
+	course = get_object_or_404(Course,pk = id)
+	courseAdmin =get_object_or_404(CourseMembership,course__id__exact =id,userType='OW').user
+
+	courseMods =[]
+	try:
+		courseWithMods = CourseMembership.objects.filter(course__id__exact = id,userType="MO")
+		courseMods = [course.user for course in courseWithMods]
+	except CourseMembership.DoesNotExist:
+		pass
+	return render_to_response('courses/course.html',{
+		'course':course,
+		'courseAdmin':courseAdmin,
+		'courseMods':courseMods,
+		})
